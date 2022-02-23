7ef253f590c3376028fca998ab3cc1f2c499e437
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2022-02-23 10:49:44 +0530

    folder and boiler plate

---
+++ templates/survey-header-boilerplate.js
@@ -0,0 +1,7 @@
+<div>
+    <code id="myCode" style="display:none">
+        setTimeout(function (){$(function () {
+            $('.footer_text').insertAfter('#surveyResponseFormId');
+        })}, 10);
+    </code><iframe style="display:none" src="about:blank" onload="eval($('#myCode').text())"></iframe>
+</div>
