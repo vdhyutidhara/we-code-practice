613604abb1bf989bca5480a6692f015a18e17745
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2022-03-15 18:13:54 +0530

    font face

---
+++ templates/using-font.js
@@ -0,0 +1,4 @@
+@font-face {
+    font-family: 'MyriadPro-Regular';
+    src: url('https://static-assets-services.s3.ap-south-1.amazonaws.com/MyriadPro-Regular.otf') format('opentype');
+}
--- z-test/script.js
+++ z-test/script.js
@@ -1,17 +1,39 @@
-function copyToClipboard() {
-    var copyText = "waszexdfcgvhb";
-    navigator.clipboard.writeText(copyText);
-    alert("Copied the Coupon Code: " + copyText);
-}
-
-
-setTimeout(function () {
-    document.querySelector("#yes-btn").addEventListener("click", copyCode);
-    function copyCode() {
-        console.log("call copy")
-        var copyText = "TIV15";
-        copyText.select();
-        document.execCommand("copy");
-        alert("Copied the Coupon Code: " + copyText);
+webengage.survey.onSubmit(function (data) {
+  if (data.surveyId === "7djkebm") {
+    var email;
+    var phone;
+    console.log(data);
+    for (var i = 0; i < data.questionResponses.length; i++) {
+      if (
+        Object.prototype.toString.call(
+          data.questionResponses[i].value.values
+        ) == "[object Object]"
+      ) {
+        for (var keys in data.questionResponses[i].value.values) {
+          if (keys == "Enter your email") {
+            email = data.questionResponses[i].value.values[keys];
+          }
+          if (keys == "Enter your mobile") {
+            phone = data.questionResponses[i].value.values[keys];
+          }
+        }
+      }
     }
-}, 1000);
+    console.log("we_email", email);
+    console.log("we_phone", phone);
+    if (webengage && webengage.state && typeof webengage.state.getForever === "function" &&
+      (webengage.state.getForever().cuid === null || webengage.state.getForever().cuid === undefined) &&
+      phone !== null
+    ) {
+      webengage.user.login(email);
+      webengage.user.setAttribute({
+        "we_email": email,
+        "we_phone": phone
+      });
+      webengage.track('lead_captured', {
+        "we_email": email,
+        "we_phone": phone
+      });
+    }
+  }
+});
